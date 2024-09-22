69c99529858585923712c0ebb46f0f5578933de3
Byeong Min Cho <merrybmc@gmail.com>
2024-09-23 00:29:30 +0900

    ui styling

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,12 +1,20 @@
 import SearchableLayout from '@/components/searchable-layout';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
-
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
 export default function Page() {
-  const router = useRouter();
+  // const router = useRouter();
+  // const { q } = router.query;
+  // return <div>검색 결과 : {q}</div>;
 
-  const { q } = router.query;
-  return <div>검색 결과 : {q}</div>;
+  return (
+    <div>
+      {books.map((books) => (
+        <BookItem key={books.id} {...books} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
