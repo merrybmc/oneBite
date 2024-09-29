5eaddbae51e67ebcafc9e79aa4b93368fa2751a6
Byeong Min Cho <merrybmc@gmail.com>
2024-09-30 00:00:01 +0900

    SSR book detail get

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router';
 import style from './[id].module.css';
+import { getServerSideProps } from './../index';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import fetchOneBook from '@/lib/fetch-one-book';
 
 const mockData = {
   id: 1,
@@ -12,10 +15,19 @@ const mockData = {
   coverImgUrl: 'https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg',
 };
 
-import React from 'react';
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { id } = context.params!;
 
-export default function Page() {
-  const { id, title, subTitle, description, author, publisher, coverImgUrl } = mockData;
+  const book = await fetchOneBook(Number(id));
+  return {
+    props: { book },
+  };
+};
+
+export default function Page({ book }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) return '문제가 발생하였습니다. 다시 시도해주세요.';
+
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } = book;
   return (
     <div className={style.container}>
       <div
