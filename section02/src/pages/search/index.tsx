1ea80c9658a85a1b4f954aeb5832276edc2ba887
Byeong Min Cho <merrybmc@gmail.com>
2024-09-29 23:55:27 +0900

    SSR books search get

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -6,15 +6,33 @@ import movies from '@/mock/dummy.json';
 import BookItem from '@/components/book-item';
 import MovieItem from '@/components/movie-item';
 import style from './index.module.css';
-export default function Page() {
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  // context
+  // 브라우저로부터 받은 요청에 대한 모든 정보 포함 (query 포함)
+
+  const { q } = context.query;
+
+  const books = await fetchBooks(q as string);
+  return {
+    props: { books },
+  };
+};
+export default function Page({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const { q } = router.query;
+  console.log('q', q);
   // return <div>검색 결과 : {q}</div>;
 
   return (
     <div className={style.search}>
-      {movies.map((movies) => (
+      {/* {movies.map((movies) => (
         <MovieItem key={movies.id} {...movies} tabState='bigTile' />
+      ))} */}
+      {books.map((books) => (
+        <BookItem key={books.id} {...books} />
       ))}
       {/* {books.map((books) => (
         <BookItem key={books.id} {...books} />
