f72b334a9d9c4acc85e00789296aa7b44b6ecbfc
Byeong Min Cho <merrybmc@gmail.com>
2024-09-29 23:47:46 +0900

    SSR books get api

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -6,39 +6,62 @@ import BookItem from '@/components/book-item';
 import movies from '@/mock/dummy.json';
 import MovieItem from '@/components/movie-item';
 import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
 // SSR 사전 렌더링
 // 해당 페이지는 앞으로 SSR로 렌더링
-export const getServerSideProps = () => {
+export const getServerSideProps = async () => {
   // 페이지 역할을 컴포넌트보다 먼저 실행이 되어서
   // 해당 컴포넌트에 필요한 데이터를 미리 불러오는 함수
   // 사전 렌더링을 하는 과정에서 딱 한 번만 실행
   // 서버 측에서만 실행이 되는 함수
 
-  const data = 'Hello';
+  // const data = 'Hello';
+
+  // return {
+  //   props: {
+  //     data,
+  //   },
+  // };
+
+  // const allBooks = await fetchBooks();
+  // const recoBooks = await fetchRandomBooks();
+
+  // 병렬로 불러오기
+  const [allBooks, recoBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
 
   return {
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
 
 // InferGetServerSidePropsType
 // getServerSideProps 함수의 반환 값의 타입을 자동으로 추론
-export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   useEffect(() => {
     // SSR은 mount가 된 이후 window를 console로 확인 가능
     console.log(window);
+    console.log(allBooks);
+    console.log(recoBooks);
   }, []);
 
   return (
     <div className={style.container}>
-      <p>{data}</p>
+      {/* <p>{data}</p> */}
       <h3>지금 가장 추천하는 영화</h3>
       <section className={style.recommendtab}>
-        {movies.map((movie) => (
+        {/* {movies.map((movie) => (
           <MovieItem tabState='bigTile' key={movie.id} {...movie} />
+        ))} */}
+        {recoBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
         ))}
         {/* {books.map((book) => (
           <BookItem key={book.id} {...book} />
@@ -46,8 +69,11 @@ export default function Home({ data }: InferGetServerSidePropsType<typeof getSer
       </section>
       <h3>등록된 모든 영화</h3>
       <section className={style.alltab}>
-        {movies.map((movie) => (
+        {/* {movies.map((movie) => (
           <MovieItem key={movie.id} {...movie} />
+        ))} */}
+        {recoBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
         ))}
         {/* {books.map((book) => (
           <BookItem key={book.id} {...book} />
